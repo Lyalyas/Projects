5f2c8cc0ec0ae2cfd41eb7f5812c8b4ab0f5f274
Lyalyas <108937693+Lyalyas@users.noreply.github.com>
2023-03-11 11:27:57 +0300

    Add files via upload

--- laba3/Лаба 3/pars.py
+++ laba3/Лаба 3/pars.py
@@ -1,3 +1,4 @@
+from openpyxl import load_workbook
 import requests
 from bs4 import BeautifulSoup as b
 import json
@@ -5,6 +6,12 @@ import csv
 import random
 from time import sleep
 import time
+import datetime
+from openpyxl.chart import PieChart, Reference, Series, BarChart
+
+fn = 'data.xlsx'
+wb=load_workbook(fn)
+ws = wb['Data']
 
 URL = "https://www.myfxbook.com/forex-broker-quotes/xm-group/2824"
 URL1 = 'https://www.myfxbook.com/ru/forex-broker-quotes/fxpro/5168'
@@ -14,9 +21,9 @@ headers = {
 	"Accept": "*/*",
 	"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15"
 }
-for i in range(100):
+for i in range(5):
 	
-	time.sleep(30)
+	
 	print("iteration ", i)
 	r = requests.get(URL, headers=headers) #@ Создаем переменную, в которой будем хранить результат get- запроса к странице.
 	r1 = requests.get(URL1, headers=headers)
@@ -36,11 +43,32 @@ for i in range(100):
 # 	# b=c.find_all(
 # 	# 	"a", {"class": "no-hover noCursorPointer"})
 # 	# print(b)
+	dt_now = datetime.datetime.now()
+	XMP_group=float(soup.find("td", {"id": "2824_1Ask"}).text)
+	FX_pro = float(soup1.find("td", {"id": "5168_1Ask"}).text)
+	one_C = float(soup2.find("td", {"id": "2320_1Ask"}).text)
+	sr_num = (float(XMP_group) + float(FX_pro)+ float(one_C)) / 3
+	ws.append({'B':XMP_group, 'C':FX_pro, 'D':one_C, 'E':sr_num, 'F':dt_now})
 
-	XMP_group=soup.find("td", {"id": "2824_1Ask"}).text
-	FX_pro = soup1.find("td", {"id": "5168_1Ask"}).text
-	FOREX = soup2.find("td", {"id": "2320_1Ask"}).text
-	print(XMP_group, FX_pro, FOREX)
 
+	print(XMP_group, FX_pro, one_C)
 
-
+	time.sleep(4)
+values = Reference(worksheet=ws,
+                 min_row=1,
+                 max_row=100,
+                 min_col=2,
+                 max_col=4)
+# создаем объект столбчатой диаграммы
+chart = BarChart()
+# добавляем в диаграмму выбранный диапазон значений
+chart.add_data(values, titles_from_data=True)
+# привязываем диаграмму к ячейке `E15`
+ws.add_chart(chart, "F11")
+# определяем размеры диаграммы в сантиметрах
+chart.width = 20
+chart.height = 5
+# сохраняем и смотрим что получилось
+wb.save(fn)
+wb.save(fn)
+wb.close()
